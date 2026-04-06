46aab519c19177aa0566256506bb6576c6d008d4
Pavel Roslovets <p.v.roslovets@gmail.com>
2026-04-06 10:18:28 +0200

    feat: make assistant

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
-from dotenv import load_dotenv
-
-from edu_assistant.config import Config
-from edu_assistant.llm_client import get_llm_client
-
-# Load environment variables from .env file
-load_dotenv()
+from edu_assistant.assistant import create_response
 
 # Prepare prompt for LLM
 INPUT_PROMPT = "Кто ты?"
 
-# Read config from YAML file
-config = Config.from_yaml_file("config.yml")
-llm_config = config.llms["api"]
-
-# Create llm_client providing llm_config from config
-llm_client = get_llm_client(llm_config=llm_config)
-
-# Call llm_client Responses API with input prompt
-# Docs: https://developers.openai.com/api/reference/resources/responses/methods/create
-response = llm_client.responses.create(
-    model=llm_config.model,
-    input=INPUT_PROMPT,
-    max_output_tokens=llm_config.max_output_tokens,
+# Call assistant
+response = create_response(
+    llm_key="api",
+    role="math_tutor",
+    template="tutor_quick_answer",
+    prompt=INPUT_PROMPT,
 )
 
-# Print the result (output_text field)
-print(response.output_text)
+print(response)
---
+++ src/edu_assistant/assistant.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+from loguru import logger
+
+from edu_assistant.config import Config, RoleType, TemplateType
+from edu_assistant.llm_client import get_llm_client
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+def create_response(
+    llm_key: str,
+    role: RoleType,
+    template: TemplateType,
+    prompt: str,
+) -> str:
+    config = Config.from_yaml_file("config.yml")
+    llm_config = config.llms[llm_key]
+    llm_client = get_llm_client(llm_config=llm_config)
+    instructions = config.render_system_instructions(role=role, template=template)
+    logger.debug(f"LLM instructions: {instructions}")
+    response = llm_client.responses.create(
+        model=llm_config.model,
+        instructions=instructions,
+        input=prompt,
+        max_output_tokens=llm_config.max_output_tokens,
+    )
+    return response.output_text
